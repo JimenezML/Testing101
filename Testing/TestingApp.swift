8be0ed3ab9f6bd989c05fb0e34e3c5919fa240d6
JimenezML <junkjunky76@gmail.com>
2024-08-07 18:34:48 -0700

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Iggi Jimenez on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
